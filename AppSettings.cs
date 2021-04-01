7f54e0bf18caae4500c2fa262dba9bd4f7f414cf
Jerry <yzi@outlook.com>
2021-04-02 00:26:41 +0900

    convert to net core

---
+++ AppSettings.cs
@@ -0,0 +1,31 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace SupSubtitleParser
+{
+    public class AppSettings
+    {
+        public string inputFile { get; set; }
+        public string outputPath { get; set; }
+        public ImageFormat imageFormat { get; set; }
+        public Color BGColor { get; set; } = Color.Transparent;
+
+        public Border border { get; set; } = new Border();
+
+        public string GetImgExt()
+        {
+            return imageFormat.ToString().ToLower().Replace("jpeg", "jpg");
+        }
+    }
+
+    public class Border
+    {
+        public Color color { get; set; } = Color.Transparent;
+        public int Width { get; set; }
+        public int Padding { get; set; }
+
+        public bool HasBorder => Padding > 0 || Width > 0;
+
+        public int EdgeWidth => Width + Padding;
+    }
+}
